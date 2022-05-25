95c38d1ba85155fd057bc785a11473e76b4488d4
trueeh-slave <steamtruesnowace@gmail.com>
2022-05-25 16:51:55 -0500

    getCollection method full

--- src/main/java/co/edu/unbosque/closedsea/resources/CollectionResource.java
+++ src/main/java/co/edu/unbosque/closedsea/resources/CollectionResource.java
@@ -5,10 +5,13 @@ import co.edu.unbosque.closedsea.services.CollectionService;
 import jakarta.servlet.ServletContext;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
@@ -41,8 +44,10 @@ public class CollectionResource {
         }
         return Response.ok().entity(collections).build();
     }
-
-    public Response createCollection(@PathParam("username") String username, @FormParam("collection") String collection, @FormParam("category") String category){
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createCollection(@PathParam("username") String username, @FormParam("collection") String collection, @FormParam("category") String category) {
         Connection conn = null;
         List<Collection> collections = null;
         Collection collectionobj = null;
@@ -68,6 +73,40 @@ public class CollectionResource {
             }
         }
         return Response.created(UriBuilder.fromResource(UserResource.class).path(username).build()).entity(collectionobj).build();
+    }
+
+    @GET
+    @Path("/{collection}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCollection(@PathParam("username") String username, @PathParam("collection") String collection){
+        Connection conn = null;
+        Collection collectionobj = null;
+
+        try{
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            CollectionService collectionService = new CollectionService(conn);
+            collectionobj = collectionService.getCollection(username,collection,"default");
+            conn.close();
+        } catch (SQLException se){
+            se.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        } finally {
+            try{
+                if(conn != null){
+                    conn.close();
+                }
+            } catch (SQLException se){
+                se.printStackTrace();
+            }
+        }
+        if(collectionobj != null){
+            return Response.ok().entity(collectionobj).build();
+        } else {
+            return Response.status(404).entity("Collection not found").build();
+        }
     }
 
 }
